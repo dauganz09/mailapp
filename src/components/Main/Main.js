ec85b9aff80482b8c190c5c7d1530973f666b111
ChanReact <christian.xircus@gmail.com>
2021-10-21 18:55:14 +0800

    Modify codebase

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import { useState,useContext,useEffect } from 'react';
+import { useState,useContext } from 'react';
 import './Main.css'
 import { MdSearch,MdFilterAlt ,MdClose} from "react-icons/md";
 import {MailItem} from '../index';
